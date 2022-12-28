812647a8708e1bbb5ad851066c6e7213c3bbf7d4
Gustavo <elmergustavo79@gmail.com>
2022-12-27 19:41:42 -0600

    update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,8 +7,8 @@ const Navbar: React.FC = () => {
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
       <Flex align="center">
-        <Image src="/images/redditFace.svg" height="30px" />
-        <Image src="/images/redditText.svg" height="46px"
+        <Image alt="image icon" src="/images/redditFace.svg" height="30px" />
+        <Image alt="image font" src="/images/redditText.svg" height="46px"
         display={{base: "none", md:"unset"}}
         />
       </Flex>
