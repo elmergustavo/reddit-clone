2fdbb91df2c823888e5c28660d28ffc4a8da4d12
Gustavo <elmergustavo79@gmail.com>
2022-12-28 08:55:59 -0600

    corregir library RecoilRoot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,14 @@ import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </RecoilRoot>
+    </>
   );
 }
