e1eb05aa22b81630c62e89a2cb931dd5a33c84ca
Gustavo <elmergustavo79@gmail.com>
2022-12-27 22:21:25 -0600

    correccion de icons

--- src/components/Navbar/Searchinput.tsx
+++ src/components/Navbar/Searchinput.tsx
@@ -1,5 +1,6 @@
+
+
 import { SearchIcon } from "@chakra-ui/icons";
-import { Icon } from "@chakra-ui/react";
 
 import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import React from "react";
@@ -12,31 +13,18 @@ const Searchinput: React.FC<SearchinputProps> = () => {
   return (
     <Flex
       flexGrow={1}
-      // maxWidth={user ? "auto" : "600px"}
+     // maxWidth={user ? "auto" : "600px"}
       mr={2}
       alignItems="center"
     >
-      <InputGroup >
+      <InputGroup>
         <InputLeftElement
           pointerEvents="none"
           color="gray.400"
-          children={
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19.6 21L13.3 14.7C12.8 15.1 12.225 15.4167 11.575 15.65C10.925 15.8833 10.2333 16 9.5 16C7.68333 16 6.146 15.371 4.888 14.113C3.62933 12.8543 3 11.3167 3 9.5C3 7.68333 3.62933 6.14567 4.888 4.887C6.146 3.629 7.68333 3 9.5 3C11.3167 3 12.8543 3.629 14.113 4.887C15.371 6.14567 16 7.68333 16 9.5C16 10.2333 15.8833 10.925 15.65 11.575C15.4167 12.225 15.1 12.8 14.7 13.3L21 19.6L19.6 21ZM9.5 14C10.75 14 11.8127 13.5627 12.688 12.688C13.5627 11.8127 14 10.75 14 9.5C14 8.25 13.5627 7.18733 12.688 6.312C11.8127 5.43733 10.75 5 9.5 5C8.25 5 7.18733 5.43733 6.312 6.312C5.43733 7.18733 5 8.25 5 9.5C5 10.75 5.43733 11.8127 6.312 12.688C7.18733 13.5627 8.25 14 9.5 14Z"
-                fill="black"
-              />
-            </svg>
-          }
-        />
-        {/* <Search2Icon mb={2} /> */}
-
+          // children={<SearchIcon mb={2} />}
+        >
+          <SearchIcon mb={2} />
+        </InputLeftElement>
         <Input
           placeholder="Search Reddit"
           fontSize="10pt"
@@ -55,26 +43,6 @@ const Searchinput: React.FC<SearchinputProps> = () => {
           bg="gray.50"
         />
       </InputGroup>
-
-      <Icon viewBox="0 0 200 200" color="red.500">
-        <path
-          fill="currentColor"
-          d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
-        />
-      </Icon>
-
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M19.6 21L13.3 14.7C12.8 15.1 12.225 15.4167 11.575 15.65C10.925 15.8833 10.2333 16 9.5 16C7.68333 16 6.146 15.371 4.888 14.113C3.62933 12.8543 3 11.3167 3 9.5C3 7.68333 3.62933 6.14567 4.888 4.887C6.146 3.629 7.68333 3 9.5 3C11.3167 3 12.8543 3.629 14.113 4.887C15.371 6.14567 16 7.68333 16 9.5C16 10.2333 15.8833 10.925 15.65 11.575C15.4167 12.225 15.1 12.8 14.7 13.3L21 19.6L19.6 21ZM9.5 14C10.75 14 11.8127 13.5627 12.688 12.688C13.5627 11.8127 14 10.75 14 9.5C14 8.25 13.5627 7.18733 12.688 6.312C11.8127 5.43733 10.75 5 9.5 5C8.25 5 7.18733 5.43733 6.312 6.312C5.43733 7.18733 5 8.25 5 9.5C5 10.75 5.43733 11.8127 6.312 12.688C7.18733 13.5627 8.25 14 9.5 14Z"
-          fill="black"
-        />
-      </svg>
     </Flex>
   );
 };
